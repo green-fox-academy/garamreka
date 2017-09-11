bb2f20631492368fe209fa5ba8383017424d2534
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 14:19:19 +0200

    BMI exercise done

---
+++ 2.week/1.day/BMI/BMI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massInKg = 52;
+            double heightInM = 1.62;
+            Console.WriteLine("My BMI is: " + massInKg / (heightInM * heightInM));
+            Console.ReadLine();
+        }
+    }
+}
