01a80e95c2587ba69a6af5497fc80e82ca4688bc
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 13:14:10 +0200

    HelloOthers exercise done

---
+++ 2.week/1.day/03 - HelloOthers/03 - HelloOthers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03___HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Norbi!");
+            Console.WriteLine("Hello Dorka!");
+            Console.WriteLine("Hello Adri!");
+            Console.ReadLine();
+
+        }
+    }
+}
