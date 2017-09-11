51be28ae72b209ef20ca595b46191bb376459dd1
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 16:31:03 +0200

    MileToKmConverter exercise done

---
+++ 2.week/1.day/15 - MileToKmConverter/15 - MileToKmConverter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _15___MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringKm;
+            Console.WriteLine("Write distance in km:");
+            stringKm = Console.ReadLine();
+            int km = Int32.Parse(stringKm);
+            double mile = km * 0.621371192;
+            Console.WriteLine("It's " + mile + " mile");
+            Console.ReadLine();
+        }
+    }
+}
