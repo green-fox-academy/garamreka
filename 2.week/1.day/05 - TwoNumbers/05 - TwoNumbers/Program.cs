b3a4f955a7693510fcd7b783c3b2c415b6600542
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 13:32:40 +0200

    TwoNumbers exercise done

---
+++ 2.week/1.day/05 - TwoNumbers/05 - TwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05___TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double thirteen = 13;
+            double twentytwo = 22;
+
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(22 - 13);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(twentytwo / thirteen);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+            Console.ReadLine();
+        }
+    }
+}
