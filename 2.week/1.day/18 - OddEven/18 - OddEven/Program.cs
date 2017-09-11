243a4a262e95667d67b3cc69b9ed852b4e8c0430
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:01:29 +0200

    OddEven exercise done

---
+++ 2.week/1.day/18 - OddEven/18 - OddEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _18___OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write a number: ");
+            int num = Int32.Parse(Console.ReadLine());
+            if (num % 2 != 0)
+            {
+                Console.WriteLine("Odd");
+            } else
+            {
+                Console.WriteLine("Even");
+            }
+            Console.ReadLine();
+        }
+    }
+}
