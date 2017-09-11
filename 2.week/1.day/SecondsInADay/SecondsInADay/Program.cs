7e5d2cc8492429f64c8c9eb03b64d3cb2136bbda
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 16:12:27 +0200

    SecondsInADay exercise done

---
+++ 2.week/1.day/SecondsInADay/SecondsInADay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SecondsInADay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            int dailySeconds = 86400;
+            int remainSeconds = dailySeconds - (currentSeconds + (currentMinutes * 60) + (currentHours * 3600));
+
+            Console.WriteLine("Remaining seconds: " + remainSeconds);
+            Console.ReadLine();
+        }
+    }
+}
