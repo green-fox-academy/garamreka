8c440ce575c13073a4856bce55af4c2f046ef2db
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 13:57:59 +0200

    FavoriteNumbers exercise done

---
+++ 2.week/1.day/07 - FavoriteNumber/07 - FavoriteNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _07___FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favorite = 2;
+            Console.WriteLine("My favorite number is: " + favorite);
+            Console.ReadLine();
+        }
+    }
+}
