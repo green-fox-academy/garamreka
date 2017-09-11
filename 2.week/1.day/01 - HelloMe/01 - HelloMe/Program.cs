6132d28baedc9737fe24ae4dbb3a88ee2f34f34d
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 12:50:09 +0200

    HelloMe exercise done

--- 2.week/1.day/01 - HelloMe/01 - HelloMe/Program.cs
+++ 2.week/1.day/01 - HelloMe/01 - HelloMe/Program.cs
@@ -6,7 +6,8 @@ namespace _01___HelloMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Reka");
+            Console.ReadLine();
         }
     }
 }
