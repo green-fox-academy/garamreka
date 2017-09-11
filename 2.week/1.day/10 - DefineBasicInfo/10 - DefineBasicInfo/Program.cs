ee8dafc9f5e14bdf29f5dadc4795ee3712ba3bec
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 14:39:36 +0200

    DefineBasicInfo exercise done

---
+++ 2.week/1.day/10 - DefineBasicInfo/10 - DefineBasicInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10___DefineBasicInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Reka";
+            int age = 27;
+            double height = 1.62;
+            bool married = false;
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am " + age);
+            Console.WriteLine("I am " + height + " m");
+            Console.WriteLine("Married: " + married);
+            Console.ReadLine();
+        }
+    }
+}
