c396af0d62d797e7790901d99f5664d5124043ef
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:07:05 +0200

    OneToALot exercise done

---
+++ 2.week/1.day/19 - OneToALot/19 - OneToALot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _19___OneToALot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write a number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num <= 0)
+            {
+                Console.WriteLine("Not enough");
+            } else if (num == 1)
+            {
+                Console.WriteLine("One");
+            } else if (num == 2)
+            {
+                Console.WriteLine("Two");
+            } else
+            {
+                Console.WriteLine("A lot");
+            }
+            Console.ReadLine();
+        }
+    }
+}
