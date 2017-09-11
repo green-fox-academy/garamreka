d63c2f5eabae6749c3b615c3bf50c18d3f1fdf44
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 16:21:48 +0200

    HelloUser exercise done

---
+++ 2.week/1.day/14 - HelloUser/14 - HelloUser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _14___HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user;
+            Console.WriteLine("Hi, what is your name?");
+            user = Console.ReadLine();
+            Console.WriteLine("Hello " + user + "!");
+            Console.ReadLine();
+        }
+    }
+}
