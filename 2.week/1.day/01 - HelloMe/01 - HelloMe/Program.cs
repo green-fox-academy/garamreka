58d602b65c344e2a77de5281d565b8756682a1ba
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 12:08:00 +0200

    dir 2.week created

---
+++ 2.week/1.day/01 - HelloMe/01 - HelloMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01___HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
