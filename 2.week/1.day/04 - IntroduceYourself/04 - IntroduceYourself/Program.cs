9fee5e89de0e9251cb86819e5f5e0661f31c3a00
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 13:22:50 +0200

    IntroduceYourself exercise done

---
+++ 2.week/1.day/04 - IntroduceYourself/04 - IntroduceYourself/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04___IntroduceYourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Reka";
+            int age = 27;
+            double height = 1.62;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.ReadLine();
+        }
+    }
+}
