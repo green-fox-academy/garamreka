69e87476c113115b949e3e77cf390605853b446a
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:22:17 +0200

    IWontCheatOnTheExam exercise done

---
+++ 2.week/1.day/23 - IWontCheatOnTheExam/23 - IWontCheatOnTheExam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _23___IWontCheatOnTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i + " I won't cheat on the Exam!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
