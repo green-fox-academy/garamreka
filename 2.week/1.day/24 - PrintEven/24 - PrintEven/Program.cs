bfe63cebb4bca0842e932e12e7710ca24611cbcb
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:26:23 +0200

    PrintEven exercise done

---
+++ 2.week/1.day/24 - PrintEven/24 - PrintEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _24___PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 500; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
