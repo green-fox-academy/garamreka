53d22558c3ecc5cc0231510b0cdbd45f736ed23b
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 16:38:03 +0200

    AnimalsAndLegs exercise done

---
+++ 2.week/1.day/16 - AnimalsAndLegs/16 - AnimalsAndLegs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _16___AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chicken;
+            Console.WriteLine("How many chickens do you have?");
+            chicken = Console.ReadLine();
+            int intChicken = Int32.Parse(chicken);
+            string pig;
+            Console.WriteLine("How many pigs do you have?");
+            pig = Console.ReadLine();
+            int intPig = Int32.Parse(pig);
+            int legs = (intChicken * 2) + (intPig * 4);
+            Console.WriteLine("Total number of legs of your animals: " + legs);
+            Console.ReadLine();
+        }
+    }
+}
