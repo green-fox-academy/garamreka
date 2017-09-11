203fb0d8d01653638e265403391618d95617c84b
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 16:00:27 +0200

    Cuboid exercise done

---
+++ 2.week/1.day/12 - Cuboid/12 - Cuboid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12___Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20;
+            double c = 30;
+
+            double surface = 2 * ((a * b) + (b * c) + (c * a));
+            double volume = a * b * c;
+
+            Console.WriteLine(surface);
+            Console.WriteLine(volume);
+            Console.ReadLine();
+        }
+    }
+}
