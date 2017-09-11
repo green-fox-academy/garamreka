4a2c3d09e8b70bc14b77f8482eb4ecbc536c9fff
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:32:20 +0200

    MultiplicationTable exercise done

---
+++ 2.week/1.day/25 - MultiplicationTable/25 - MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _25___MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " * " + num + " = " + (i * num));
+            }
+            Console.ReadLine();
+        }
+    }
+}
