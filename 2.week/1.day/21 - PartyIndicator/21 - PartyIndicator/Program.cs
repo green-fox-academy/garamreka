e94b151fcfd7f560cdee3a510c8cf0216916ddfd
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 17:17:58 +0200

    PartyIndicator exercise done

---
+++ 2.week/1.day/21 - PartyIndicator/21 - PartyIndicator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _21___PartyIndicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of girls: ");
+            int girls = int.Parse(Console.ReadLine());
+            Console.Write("Number of boys: ");
+            int boys = int.Parse(Console.ReadLine());
+
+            if (girls == 0)
+            {
+                Console.Write("Sausage party");
+            }
+            else if (girls == boys && (girls + boys) >= 20)
+            {
+                Console.Write("The party is excellent");
+            }
+            else if ((girls + boys) >= 20 && (girls > boys || boys > girls))
+            {
+                Console.Write("Quite cool party");
+            }
+            else if ((girls + boys) < 20)
+            {
+                Console.Write("Average party...");
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
