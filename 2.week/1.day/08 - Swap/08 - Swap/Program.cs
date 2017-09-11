bb2d55f2893df30b3ab843145fdc976df6258227
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-11 14:13:18 +0200

    Swap exercise done

---
+++ 2.week/1.day/08 - Swap/08 - Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08___Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("New a is: " + a);
+            Console.WriteLine("New b is: " + b);
+            Console.ReadLine();
+        }
+    }
+}
