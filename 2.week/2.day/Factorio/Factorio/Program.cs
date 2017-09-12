beb66e87a8dba4fb178e81a88f59060e0ecd672f
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 13:51:23 +0200

    Factorio exercise done

---
+++ 2.week/2.day/Factorio/Factorio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorio(5));
+            Console.ReadLine();
+        }
+
+        public static int Factorio (int num)
+        {
+            int result = 1;
+            for (var i = num; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
