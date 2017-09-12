cae0524e6e53d0a748167241e5f56ff2a5da3a65
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 13:30:49 +0200

    AppendA exercise done

---
+++ 2.week/2.day/AppendA/AppendA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            Console.WriteLine(AppendA(am));
+            Console.ReadLine();
+        }
+
+        public static string AppendA(string character)
+        {
+            character = character + "a";
+            return character;
+        }
+    }
+}
