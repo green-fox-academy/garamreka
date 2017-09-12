2c9183bb7d0667afe405677bc7ccdcd487715245
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 13:25:18 +0200

    Greet exercise done

---
+++ 2.week/2.day/Greet/Greet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            Console.WriteLine(Greet(al));
+            Console.ReadLine();
+        }
+
+        public static string Greet(string user)
+        {
+            user = "Greetings dear, " + user;
+            return user;
+        }
+    }
+}
