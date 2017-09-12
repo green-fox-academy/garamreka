c175334530118609dee5c878dec23b1514955e80
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 17:34:56 +0200

    Printer exercise created

---
+++ 2.week/2.day/Printer/Printer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] multiString = { "first", "second" };
+            Console.WriteLine(Printer(args: multiString));
+            Console.ReadLine();
+        }
+
+        public static void Printer(params string[] args)
+        {
+            Console.WriteLine(args);
+           
+        }
+    }
+}
