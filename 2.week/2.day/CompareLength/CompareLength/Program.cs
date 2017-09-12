0c091b169df99ac0c1be1f5d08ea35cea64ec47a
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 17:14:54 +0200

    CompareLength exercise done

---
+++ 2.week/2.day/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5, 6, 7 };
+            if (p2.Length > p1.Length)
+            {
+                    Console.Write("p2 has more element");
+                    Console.ReadLine();
+            }
+            
+        }
+    }
+}
