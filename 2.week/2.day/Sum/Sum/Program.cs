acc4eeab98dfd14fc6352f72fcbea175863379b0
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-12 13:44:54 +0200

    Sum exercise done

---
+++ 2.week/2.day/Sum/Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(4));
+            Console.ReadLine();
+        }
+
+        public static int Sum(int num)
+        {
+            int result = 0;
+            for (var i = num; i > 0; i--)
+            {
+             result += i;
+            }
+            return result;
+        }
+    }
+}
