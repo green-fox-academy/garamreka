eff1673d2aac862bb3af9bee6ba316dcb6dc68aa
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 16:09:45 +0200

    AppendLetters exercise done

---
+++ 2.week/3.day/08 - AppendLetters/08 - AppendLetters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08___AppendLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+                Console.WriteLine(far[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
