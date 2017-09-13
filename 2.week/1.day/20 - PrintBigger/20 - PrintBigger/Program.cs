744ad6b40f4ef20b434c42f7e347f51d23bf695f
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 22:53:40 +0200

    PrintBigger exercise done

---
+++ 2.week/1.day/20 - PrintBigger/20 - PrintBigger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20___PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Give me a second number: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine(n1 + " is the bigger");
+            }
+            else if (n1 < n2)
+            {
+                Console.WriteLine(n2 + " is the bigger");
+            }
+            Console.ReadLine();
+        }
+    }
+}
