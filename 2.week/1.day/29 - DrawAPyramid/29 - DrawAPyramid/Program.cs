bf73ea624ed410df8057af151f24ded5bb7792d9
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 22:40:28 +0200

    DrawPyramid exercise done

---
+++ 2.week/1.day/29 - DrawAPyramid/29 - DrawAPyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _29___DrawAPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me the height of your pyramid: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                for (int j = 0; j < num - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int l = 0; l < num - i; l++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
