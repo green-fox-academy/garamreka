690ceb1e0285a7f56ae7a3775c360389db244cc8
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 17:15:43 +0200

    Candyshop exercise done

---
+++ 2.week/3.day/09 - Candyshop/09 - Candyshop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09___Candyshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            //list[list.FindIndex(ind => ind.Equals(2))] = "Croissant";
+            //list[list.FindIndex(ind => ind.Equals(false))] = "Ice Cream";
+
+            list[1] = "Croissant";
+            list[3] = "Ice cream";
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
