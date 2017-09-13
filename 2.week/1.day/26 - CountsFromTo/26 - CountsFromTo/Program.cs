efc97a390bec703a7248a9f05de3a5aa9a0965a9
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 21:58:39 +0200

    CountsFromTo exercise done

---
+++ 2.week/1.day/26 - CountsFromTo/26 - CountsFromTo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _26___CountsFromTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+            int n1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Give me a second number: ");
+            int n2 = Int32.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("The second number should be bigger");
+            } else
+            {
+                for (var i = n1; i < n2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
