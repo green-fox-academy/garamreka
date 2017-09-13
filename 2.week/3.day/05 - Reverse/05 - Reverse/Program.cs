31a2a9605e925bc999fd1b121b6a8f872e55ff19
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 14:43:09 +0200

    Reverse exercise done

---
+++ 2.week/3.day/05 - Reverse/05 - Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05___Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Reversed(reversed);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void Reversed(string input)
+        {
+            char[] chars = input.ToCharArray();
+
+            for (var i = chars.Length-1; i >= 0; i--)
+            {
+                Console.Write(chars[i]);
+            }
+            
+        }
+    }
+}
