c4f9fc7e29dbe67ff7e53094f6f0f1a04652dacb
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 22:16:27 +0200

    DrawTriangle exercise done

---
+++ 2.week/1.day/28 - DrawTriangle/28 - DrawTriangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _28___DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+            int num = int.Parse(Console.ReadLine());
+            string star = "";
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(star);
+                star += "*";
+            }
+            Console.ReadLine();
+        }
+    }
+}
