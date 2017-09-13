8df74975e5099edf35082b7933b3e6fd4d417666
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-13 11:55:44 +0200

    SolarSystem exercise done

---
+++ 2.week/3.day/06 - SolarSystem/06 - SolarSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06___SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5, "Saturn");
+
+            foreach (var planet in planetList)
+            {
+                Console.Write(planet + " ");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
