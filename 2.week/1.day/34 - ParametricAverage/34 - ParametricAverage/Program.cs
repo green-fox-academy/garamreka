2ec825dfc0c79eba30babf2c932923e0d11064c6
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 11:31:22 +0200

    ParametricAverage exercise done

---
+++ 2.week/1.day/34 - ParametricAverage/34 - ParametricAverage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _34___ParametricAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hi! Give me a number ");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("Give one more! ");
+                sum += int.Parse(Console.ReadLine());
+            }
+
+            double average = sum / num;
+            Console.WriteLine("Sum: " + sum + ", Average: " + average);
+            Console.ReadLine();
+        }
+    }
+}
