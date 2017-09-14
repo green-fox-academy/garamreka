dc2044b85c5a0e5a5182b825fe4f18cb838ed073
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 15:48:10 +0200

    Printer exercise refactored

--- 2.week/2.day/Printer/Printer/Program.cs
+++ 2.week/2.day/Printer/Printer/Program.cs
@@ -7,15 +7,31 @@ namespace Printer
 
         static void Main(string[] args)
         {
-            string[] multiString = { "first", "second" };
-            Console.WriteLine(Printer(args: multiString));
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+
+            string[] example1 = { "first" };
+            string[] example2 = { "first", "second" };
+            string[] example3 = { "first", "second", "third", "fourh" };
+
+            Printer(example3);
             Console.ReadLine();
         }
 
-        public static void Printer(params string[] args)
+        public static void Printer(string[] input)
         {
-            Console.WriteLine(args);
-           
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+
         }
     }
 }
