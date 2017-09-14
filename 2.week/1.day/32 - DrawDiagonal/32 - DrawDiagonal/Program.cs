62839dc0fba23a8b2ea4e07690d79f47b15c2a73
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 13:09:06 +0200

    DrawDiagonal exercise refactored

---
+++ 2.week/1.day/32 - DrawDiagonal/32 - DrawDiagonal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _32___DrawDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me the height of your square ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your square with a diagonal line!");
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("%");
+            }
+
+            int counter = 0;
+
+            Console.WriteLine();
+            for (int j = 0; j < height - 2; j++)
+            {
+                    Console.Write("%");
+                    for (int k = 0; k < height - 2; k++)
+                    {
+                        if ( k == counter)
+                        {
+                        Console.Write("%");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    counter++;
+                    Console.Write("%");
+                    Console.WriteLine();
+                }
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
