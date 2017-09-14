73e38197654ba801782c17eed942175d77bd8cc6
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 11:54:35 +0200

    DrawChessTable exercise done

---
+++ 2.week/1.day/35 - DrawChessTable/35 - DrawChessTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _35___DrawChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ChessTable");
+            Console.WriteLine();
+            for (int i = 1; i <= 8; i++)
+            {
+                if (i%2 == 1)
+                {
+                    for (int j = 1; j <= 4; j++)
+                    {
+                        Console.Write("% ");
+                    }
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    for (int k = 1; k <= 4; k++)
+                    {
+                        Console.Write(" %");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
