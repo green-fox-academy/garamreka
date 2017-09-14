8fe7a3bbdc28ad99e7ee1be073d1991d07758766
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 13:48:13 +0200

    SwapElements exercise done

---
+++ 2.week/2.day/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            string[] abc = { "first", "second", "third" };
+
+            Swap(abc);
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.WriteLine(abc[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static void Swap(string[] array)
+        {
+            var tmp = array[0];
+            array[0] = array[array.Length-1];
+            array[array.Length - 1] = tmp;
+        }
+    }
+}
