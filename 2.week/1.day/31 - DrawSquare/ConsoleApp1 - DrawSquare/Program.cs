e21cdc5d1a56a31ed93d5606a9732c0e6ef2d46e
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-14 11:03:03 +0200

    DrawSquare exercise done

---
+++ 2.week/1.day/31 - DrawSquare/ConsoleApp1 - DrawSquare/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1___DrawSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me the height of your square ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your square!");
+
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("%");
+                }
+                Console.WriteLine();
+                for (int k = 0; k < height-2; k++)
+                {
+                    Console.Write("%");
+                    for (int l = 0; l < height-2; l++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("%");
+                    Console.WriteLine();
+                }
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("%");
+                }
+                Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
