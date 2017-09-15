57e129636cbc3176358c16fc1a808923ac21398a
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-15 10:16:20 +0200

    IsAnagram exercise done

---
+++ 2.week/5.day/IsAnagram/IsAnagram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IsAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a function named is anagram following your current language's style guide. 
+            //It should take two strings and return a boolean value depending on whether its an anagram or not.
+
+            Console.Write("Hi! Give me the two word that needs to be checked! ");
+            string[] userInput = Console.ReadLine().Split();
+            Console.WriteLine();
+
+            IsAnagram(userInput[0], userInput[1]);
+            Console.ReadLine();
+        }
+        public static void IsAnagram (string input1, string input2)
+        {
+            for (int i = input1.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < input2.Length; j++)
+                {
+                    if (input1[i] == input2[j])
+                    {
+                        Console.WriteLine(true);
+                    }
+                    else
+                    {
+                        Console.WriteLine(false);
+                    }
+                    break;
+                }
+                break;
+            }
+        }
+    }
+}
