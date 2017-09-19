60dbf5761067e7515a330ef09d65064704e95f71
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-19 11:22:48 +0200

    DivideByZero (feat): function created to handle divisor equals zero exception

---
+++ 3.week/2.day/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+
+            Console.Write("Give me a divisor: ");
+            int divisorByUser = int.Parse(Console.ReadLine());
+
+            DivideByZero(divisorByUser);
+        }
+        public static void DivideByZero (int divisor)
+        {
+            try
+            {
+                double result = 10 / divisor;
+                Console.WriteLine("Result is " + result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Fail");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
