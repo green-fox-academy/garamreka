e823b5743b77c7bc589a3d3f3af34d9aa9b6dca9
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-19 17:51:12 +0200

    complete PrintEachLine exercise

---
+++ 3.week/2.day/DailyWork2.0/DailyWork2.0/PrintEachLine.cs
@@ -0,0 +1,35 @@
+// Write a program that opens a file called "my-file.txt", then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: "Unable to read file: my-file.txt"
+using System;
+using System.IO;
+
+namespace DailyWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./my-file.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("Hello Alpaga!");
+            }
+            try
+            {
+
+                string[] content = File.ReadAllLines(path);
+                Console.WriteLine(content[0]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
