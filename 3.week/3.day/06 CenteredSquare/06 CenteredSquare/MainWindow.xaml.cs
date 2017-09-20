bba1cf50a6f05adc9b6ffbeacb527c833186b128
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-20 13:52:34 +0200

    complete CenteredSquare exercise

---
+++ 3.week/3.day/06 CenteredSquare/06 CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _06_CenteredSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a green 10x10 square to the canvas' center.
+
+            var topLeft = new Point((canvas.Width/2) - 5, (canvas.Height / 2) - 5);
+            var bottomLeft = new Point((canvas.Width / 2) - 5, (canvas.Height / 2) + 5);
+            var topRight = new Point((canvas.Width / 2) + 5, (canvas.Height / 2) - 5);
+            var bottomRight = new Point((canvas.Width / 2) + 5, (canvas.Height / 2) + 5);
+
+            foxDraw.DrawLine(topLeft, topRight);
+            foxDraw.DrawLine(topRight, bottomRight);
+            foxDraw.DrawLine(bottomRight, bottomLeft);
+            foxDraw.DrawLine(bottomLeft, topLeft);
+        }
+    }
+}
