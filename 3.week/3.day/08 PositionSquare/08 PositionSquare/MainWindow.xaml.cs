84ded63b08489b43c288fc5b32f7a5faa1ccc181
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-20 14:50:47 +0200

    complete PositionSquare exercise

---
+++ 3.week/3.day/08 PositionSquare/08 PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _08_PositionSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function.  
+
+            Random random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                foxDraw.FillColor(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+                SquareDrawer(foxDraw, random.Next(0, 250), random.Next(0, 250));
+            }
+        }
+        public void SquareDrawer(FoxDraw draw, int x, int y)
+        {
+            draw.DrawRectangle(x, y, 50, 50);
+        }
+    }
+}
