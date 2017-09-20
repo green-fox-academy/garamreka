20be1d42f947899ba612019d94abfc035d90e4c1
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-20 16:30:24 +0200

    complete PurpleSteps3D exercise

---
+++ 3.week/3.day/12 PurpleSteps3D/12 PurpleSteps3D/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _12_PurpleSteps3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+
+            foxDraw.FillColor(Colors.Purple);
+
+            double size = 10;
+            double x = 10;
+            int length = 6;
+            
+            for (int i = 1; i < length; i++)
+            {
+                foxDraw.DrawRectangle(x, x, size, size);
+                size += 10;
+                x += i * 10;
+            }
+        }
+    }
+}
