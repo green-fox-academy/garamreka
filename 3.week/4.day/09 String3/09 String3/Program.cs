a5a8a51df56b19da319f7c5a16aa8d7151d3d83e
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-21 16:16:42 +0200

    complete String3 exercise

---
+++ 3.week/4.day/09 String3/09 String3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+
+            string myString = "xoxo";
+            int stringLength = myString.Length;
+
+            Console.WriteLine(Star(myString, stringLength));
+            Console.ReadLine();
+        }
+        public static string Star(string myString, int length)
+        {
+            if (length == 0)
+            {
+                return "*";
+            }
+            else
+            {
+                return Star(myString, length - 1) + myString.Substring(length - 1, 1) + "*";
+            }
+        }
+    }
+}
