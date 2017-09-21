31b66d4e14aef46e56724f78bc129a5debea01de
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-21 10:48:51 +0200

    complete Counter exercise

---
+++ 3.week/4.day/01 Counter/01 Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Counter(10);
+            Console.ReadLine();
+        }
+        public static void Counter (int n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine(n);
+                Counter(n-1);
+            }
+        }
+    }
+}
