c894a80b6650dddd1d0d3b3ddd69524c67cdcd69
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-21 11:52:14 +0200

    complete Bunnies exercise

---
+++ 3.week/4.day/05 Bunnies/05 Bunnies/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+            Console.WriteLine(BunnyEars(10));
+            Console.ReadLine();
+        }
+        public static int BunnyEars ( int bunny)
+        {
+            if (bunny == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 2 + BunnyEars(bunny - 1);
+            }
+        }
+    }
+}
