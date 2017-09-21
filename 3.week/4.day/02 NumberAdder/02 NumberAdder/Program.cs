d92da735708160fa9662339db6a2583495135574
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-21 10:53:10 +0200

    complete NumberAdder exercise

---
+++ 3.week/4.day/02 NumberAdder/02 NumberAdder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+            Console.WriteLine(NumberAdder(5));
+            Console.ReadLine();
+        }
+        public static int NumberAdder (int n)
+        {
+            if ( n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + NumberAdder(n - 1);
+            }
+        }
+    }
+}
