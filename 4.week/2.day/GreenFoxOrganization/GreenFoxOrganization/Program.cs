bb232f3f0bc018d71acc3e501c86525d3096208f
garamreka <garam.reka@gmail.com>
2017-09-26 17:04:16 +0200

    add Sponsor class to GreenFoxOrganization exercise

--- 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Program.cs
+++ 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Program.cs
@@ -6,16 +6,26 @@ namespace GreenFoxOrganization
     {
         static void Main(string[] args)
         {
-            Person test = new Person();
-            test.Introduce();
             Person mark = new Person("Mark", 46, "male");
             mark.Introduce();
             mark.GetGoal();
+            Person jane = new Person();
+            jane.Introduce();
             Student john = new Student("John Doe", 20, "male", "BME");
             john.Introduce();
             john.GetGoal();
+            Student student = new Student();
+            student.Introduce();
             Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
             gandhi.Introduce();
+            gandhi.GetGoal();
+            Mentor mentor = new Mentor();
+            mentor.Introduce();
+            Sponsor sponsor = new Sponsor();
+            sponsor.Introduce();
+            Sponsor elon = new Sponsor("Elon Musk", 46, "male", "SpaceX");
+            elon.Introduce();
+            elon.GetGoal();
 
             Console.ReadLine();
         }
---
+++ 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Sponsor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxOrganization
+{
+    class Sponsor : Person
+    {
+        string company;
+        int hiredStudent;
+
+        public Sponsor(string name, int age, string gender, string company)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.company = company;
+            hiredStudent = 0;
+        }
+        public Sponsor()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+            company = "Google";
+            hiredStudent = 0;
+        }
+
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} who represents {3} and hired {4} students so far.", name, age, gender, company, hiredStudent);
+        }
+        public void Hire()
+        {
+            hiredStudent++;
+        }
+        public new void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+    }
+}
