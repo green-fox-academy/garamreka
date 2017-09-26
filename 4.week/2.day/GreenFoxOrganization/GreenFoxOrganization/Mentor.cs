3e600bb8366d547200f5256f781505bb771c8b90
garamreka <garam.reka@gmail.com>
2017-09-26 16:52:32 +0200

    add Mentor class to GreenFoxOrganization exercise

---
+++ 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Mentor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxOrganization
+{
+    class Mentor : Person
+    {
+        string level;
+
+        public Mentor(string name, int age, string gender, string level)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.level = level;
+        }
+        public Mentor()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+            level = "intermediate";
+        }
+
+        public new void GetGoal()
+        {
+            Console.WriteLine("Educate brilliant junior software developers.");
+        }
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} {3} mentor.", name, age, gender, level);
+        }
+    }
+}
--- 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Program.cs
+++ 4.week/2.day/GreenFoxOrganization/GreenFoxOrganization/Program.cs
@@ -10,9 +10,12 @@ namespace GreenFoxOrganization
             test.Introduce();
             Person mark = new Person("Mark", 46, "male");
             mark.Introduce();
+            mark.GetGoal();
             Student john = new Student("John Doe", 20, "male", "BME");
             john.Introduce();
             john.GetGoal();
+            Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
+            gandhi.Introduce();
 
             Console.ReadLine();
         }
