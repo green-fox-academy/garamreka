d2849f00938f00e9826db95f223794147872ba24
garamreka <garam.reka@gmail.com>
2017-09-27 15:29:04 +0200

    complete CountLetters exercise

---
+++ 4.week/3.day/CountLetters/CountLetters/CountLetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class CountLetter
+    {
+        public Dictionary<char, int> FromStringToDictionary(string input)
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>();
+
+            char[] fromString = input.ToCharArray();
+
+            foreach (var character in fromString)
+            {
+                if (!output.ContainsKey(character))
+                {
+                    output.Add(character, 1);
+                }
+                else
+                {
+                    output[character]++;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ 4.week/3.day/CountLetters/CountLettersTest/CountLetterTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetters;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CountLetterTest
+    {
+        CountLetter countLetter = new CountLetter();
+
+        [Test]
+        public void FromStringToDictionary_WhenSameChars_ThenSameKeys()
+        {
+            string input = "pallida";
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+                { 'p', 1 },
+                { 'a', 2 },
+                { 'l', 2},
+                { 'i', 1 },
+                { 'd', 1 }
+
+            };
+            Assert.AreEqual(output, countLetter.FromStringToDictionary(input));
+        }
+    }
+}
