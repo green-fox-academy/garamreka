fd569ba089b301496c01f17b2a769f94dbbf4210
garamreka <garam.reka@gmail.com>
2017-09-27 13:09:18 +0200

    create first test in Sum exercise

---
+++ 4.week/3.day/Sum/Sum/SumClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class SumClass
+    {
+        public int SumElementsOfList(List<int> intList)
+        {
+            int sumOfElements = 0;
+
+            foreach (var element in intList)
+            {
+                sumOfElements += element;
+            }
+
+            return sumOfElements;
+        }
+    }
+}
---
+++ 4.week/3.day/Sum/SumTest/SumTestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTestClass
+    {
+        SumClass sum = new SumClass();
+        [Test]
+        public void TestSumFunction()
+        {
+            List<int> input = new List<int>() { 1, 2, 3, 4, 5 };
+
+            int output = 15;
+
+            Assert.AreEqual(output, sum.SumElementsOfList(input));
+        }
+    }
+}
