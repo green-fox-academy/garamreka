2e09ff8c787a136520fdd0a3a392a5e08b61d077
garamreka <garam.reka@gmail.com>
2017-09-27 14:22:35 +0200

    create and successfully test one true case and two false cases in Anagram exercise

---
+++ 4.week/3.day/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool AreTheseTwoStringsAnagram(string input1, string input2)
+        {
+            bool isAnagram = false;
+            char[] input1CharArray = input1.ToCharArray();
+            char[] input2CharArray = input2.ToCharArray();
+
+            Array.Sort(input1CharArray);
+            Array.Sort(input2CharArray);
+
+            if (input1CharArray.Length == input2CharArray.Length && input2CharArray.SequenceEqual(input1CharArray))
+            {
+                    isAnagram = true;
+                    return isAnagram;
+            }
+
+            return isAnagram;
+        }
+    }
+}
---
+++ 4.week/3.day/Anagram/AnagramTest/AnagramTestClass.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTestClass
+    {
+        AnagramClass anagram = new AnagramClass();
+
+        [Test]
+        public void TestAnagramFunction_WhenStringsAreEqual_ThenTrue()
+        {
+            string input1 = "pallida";
+            string input2 = "plldaia";
+            bool output = true;
+
+            Assert.AreEqual(output, anagram.AreTheseTwoStringsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagramFunction_WhenStringsLengthAreNotEqual_ThenFalse()
+        {
+            string input1 = "pallida";
+            string input2 = "idea";
+            bool output = false;
+
+            Assert.AreEqual(output, anagram.AreTheseTwoStringsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagramFunction_WhenSortedStringsAreNotEqual_ThenFalse()
+        {
+            string input1 = "pallida";
+            string input2 = "pallido";
+            bool output = false;
+
+            Assert.AreEqual(output, anagram.AreTheseTwoStringsAnagram(input1, input2));
+        }
+    }
+}
