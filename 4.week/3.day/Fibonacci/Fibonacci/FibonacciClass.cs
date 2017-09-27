cc6ce93ff3938b102f3dd1b25659fc0e116ea83f
garamreka <garam.reka@gmail.com>
2017-09-27 16:34:20 +0200

    passed test in Fibonacci exercise when index is more than two

---
+++ 4.week/3.day/Fibonacci/Fibonacci/FibonacciClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class FibonacciClass
+    {
+        public double ComputeFibonacci(double index)
+        {
+            //double elementByIndex = 0;
+            int firstElement = 0;
+            int secondElement = 1;
+            
+            if (index < 1)
+            {
+                return 400;
+            }
+            if (index == 1 || index == null)
+            {
+                return firstElement;
+            }
+            if (index == 2)
+            {
+                return secondElement;
+            }
+
+            return ComputeFibonacci(index - 1) + ComputeFibonacci(index - 2);
+        }
+    }
+}
---
+++ 4.week/3.day/Fibonacci/FibonacciTest/FibonacciTestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonacci;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class FibonacciTestClass
+    {
+        FibonacciClass fibonacciByIndex = new FibonacciClass();
+
+        [Test]
+        public void ComputeFibonacci_WhenIndexMoreThanTwo_ThenGiveElement()
+        {
+            double input = 5;
+            double output = 3;
+            Assert.AreEqual(output, fibonacciByIndex.ComputeFibonacci(input));
+        }
+
+    }
+}
