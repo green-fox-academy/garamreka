aaa224383160931780e3980fb84ad3782412205e
garamreka <garam.reka@gmail.com>
2017-09-27 11:42:36 +0200

    complete first passed test in Apples exercise

---
+++ 4.week/3.day/Apples/ApplesTest/ApplesTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apples;
+
+namespace ApplesTest
+{
+    [TestFixture]
+
+    public class AppleTest
+    {
+        Apple apple = new Apple();
+
+        [Test]
+        public void TestApple_WhenApple_ThenApple()
+        {
+            //arrange
+
+            //act
+            string output = "apple";
+
+            //assert
+            Assert.AreEqual(output, apple.GetApple());
+        }
+    }
+}
