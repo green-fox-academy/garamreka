b24f8e438e2a7512423fe3d9d19c4547318c6578
garamreka <garam.reka@gmail.com>
2017-10-18 14:37:04 +0200

    complete Exercise7 with both query and lambda

---
+++ 6.week/3.day/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            foreach (var city in FindAIWithQuery(cities))
+            {
+                Console.WriteLine(city);
+            }
+
+            foreach (var city in FindAIWithLambda(cities))
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<string> FindAIWithQuery(string[] input)
+        {
+            var city =
+                (from c in input
+                where c.StartsWith("A") && c.EndsWith("I")
+                select c).ToList();
+
+            return city;
+        }
+
+        static List<string> FindAIWithLambda(string[] input)
+        {
+            var city = input.Where(c => c.StartsWith("A") && c.EndsWith("I")).ToList();
+
+            return city;
+        }
+    }
+}
