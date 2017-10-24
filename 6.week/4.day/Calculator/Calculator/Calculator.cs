1876113146af1f2cb624b84a01eca049e12df00d
garamreka <garam.reka@gmail.com>
2017-10-24 14:29:07 +0200

    experiment with generic methods

---
+++ 6.week/4.day/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculation<T>
+    {
+        static dynamic operand1;
+        static dynamic operand2;
+
+        public static T Add(string[] args)
+        {
+            operand1 = Convert.ChangeType(args[0], typeof(T));
+            operand2 = Convert.ChangeType(args[2], typeof(T));
+            return  operand1 + operand2;
+        }
+
+        //public static double Substract(T value1, T value2)
+        //{
+        //    double? operand1 = value1 as double?;
+        //    double? operand2 = value2 as double?;
+
+        //    return operand1 - operand2;
+        //}
+
+        //public static T Multiply(T[] args)
+        //{
+        //    operand1 = (double)args[0];
+        //    operand2 = (double)args[2];
+        //    return operand1 * operand2;
+        //}
+
+
+
+        //public static T Divide(T x, T y)
+        //    where T : double, int
+        //{
+        //    return x / y;
+        //}
+    }
+}
---
+++ 6.week/4.day/Calculator/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\nCALCULATOR\n" +
+                    "===========\n\n" +
+                    "[operand] [operation] [operand]");
+            }
+            else if (args.Length !=3)
+            {
+                Console.WriteLine("Oh-oh, something is missing, please write again your data\n\n" +
+                    "[operand][operation][operand]");
+            }
+
+            if (args.Contains("+"))
+            {
+                Console.WriteLine();
+                Console.WriteLine(Calculation<decimal>.Add(args));
+            }
+
+            //else if (args.Contains("/"))
+            //{
+            //    Console.WriteLine(Calculation<double>.Divide(args));
+            //}
+            //else if (args.Contains("*"))
+            //{
+            //    Calculator<double>.Multiply(args);
+            //}
+            //else if (args.Contains("-"))
+            //{
+            //    Console.WriteLine(Calculation<decimal>.Substract(args));
+            //}
+        }
+    }
+}
