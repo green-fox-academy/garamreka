f3697e59b482b521e6295a7de42a58e3d85fb262
garamreka <garam.reka@gmail.com>
2017-10-24 13:58:21 +0200

    add models folder

---
+++ 7.week/1.day/MyFirstWebApp/MyFirstWebApp/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Models
+{
+    public class Greeting
+    {
+        public long Id = 1;
+        public string Content = "Hello World";
+    }
+}
