3d70e3ce103374d6205f97518745374afebb5c65
garamreka <garam.reka@gmail.com>
2017-10-24 13:59:25 +0200

    create hello world REST App

---
+++ 7.week/1.day/MyFirstWebApp/MyFirstWebApp/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstWebApp.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            return new JsonResult(new Greeting());
+        }
+    }
+}
