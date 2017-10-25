c09e94eb7d03357b91312aa003223a34a406fef2
garamreka <garam.reka@gmail.com>
2017-10-25 14:01:09 +0200

    create BankOfSimba part I

---
+++ 7.week/3.day/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+
+        [Route("simba")]
+        public IActionResult Index()
+        {
+            var bankAccount = new BankAccount("Simba", 2000, "lion");
+            return View(bankAccount);
+        }
+    }
+}
---
+++ 7.week/3.day/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount(string name, int balance, string type)
+        {
+            this.Name = name;
+            this.Balance = balance;
+            this.AnimalType = type;
+        }
+    }
+}
