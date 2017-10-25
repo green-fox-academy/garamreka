bdf8dc2c37a8c75ce7bde0e8c4ce810adea6e965
garamreka <garam.reka@gmail.com>
2017-10-25 11:29:33 +0200

    change 1.day to 2.day

---
+++ 7.week/2.day/MyFirstWebApp/MyFirstWebApp/Controllers/WebController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstWebApp.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        public static int LoadCounter = 0;
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            var greeting = new Greeting(name)
+            {
+                Id = 1,
+                Content = name
+            };
+
+            LoadCounter++;
+            return View(greeting);
+        }
+    }
+}
---
+++ 7.week/2.day/MyFirstWebApp/MyFirstWebApp/Models/Greeting.cs
@@ -0,0 +1,28 @@
+﻿using MyFirstWebApp.Controllers;
+
+namespace MyFirstWebApp.Models
+{
+    public class Greeting
+    { 
+        public long Id { get; set; }
+        public string Content { get; set; }
+
+        public string WebPage = "(It is a web page.)";
+        public int LoadCounter = WebController.LoadCounter;
+
+        public string[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag", "Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+        public Greeting(string name)
+        {
+            Id = RESTController.idCounter;
+            Content = $"Hello, {name}";
+        }
+
+        public Greeting()
+        {
+        }
+    }
+}
