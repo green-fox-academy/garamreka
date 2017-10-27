3c42d4fdad11775c0534bd95846301d27cc369df
garamreka <garam.reka@gmail.com>
2017-10-27 11:41:39 +0200

    feat(FoxClub): setup and basic design

---
+++ 7.week/5.day/FoxClub/FoxClub/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FoxClub.Models;
+
+namespace FoxClub.Controllers
+{
+    public class HomeController : Controller
+    {
+        Fox fox;
+
+        public HomeController(Fox fox)
+        {
+            this.fox = fox;
+        }
+
+        [HttpGet]
+        [Route ("")]
+        public IActionResult Index()
+        {
+            return View(fox);
+        }
+
+        //[HttpPost]
+        //[Route("nutritionStore")]
+        //public IActionResult PostNutritionStore()
+        //{
+        //    return RedirectToAction("NutritionStore");
+        //}
+
+        [HttpGet]
+        [Route("nutritionStore")]
+        public IActionResult NutritionStore()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[Route("trickCenter")]
+        //public IActionResult PostTrickCenter()
+        //{
+        //    return RedirectToAction("TrickCenter");
+        //}
+
+        [HttpGet]
+        [Route("trickCenter")]
+        public IActionResult TrickCenter()
+        {
+            return View();
+        }
+    }
+}
---
+++ 7.week/5.day/FoxClub/FoxClub/Models/Fox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxClub.Models
+{
+    public class Fox
+    {
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Drink { get; set; }
+        public List<string> Tricks { get; set; } = new List<string>() { "write HTML", "code in C#"};
+        public int TrickCount { get; set; }
+
+        public Fox()
+        {
+            this.Name = "Mr. Fox";
+            this.Food = "pizza";
+            this.Drink = "lemonade";
+            this.TrickCount = Tricks.Count;
+        }
+    }
+}
