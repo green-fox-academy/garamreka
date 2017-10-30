c419ff47f600e4474c595c12b2881278dc3e374d
garamreka <garam.reka@gmail.com>
2017-10-30 10:00:04 +0100

    GreetSomeone: demo related modification (change design)

--- 7.week/4.day/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
+++ 7.week/4.day/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using GreetSomeone.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace GreetSomeone.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-
         Greeting greeting;
 
         public HomeController(Greeting greeting)
