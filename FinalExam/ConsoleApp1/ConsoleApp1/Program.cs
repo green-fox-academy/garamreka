4eb6d050265b66a996a410a21b0bf51a98356023
garamreka <garam.reka@gmail.com>
2018-01-15 10:31:40 +0100

    Final Exam Mini Exercise solution

---
+++ FinalExam/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OldList = new List<string>()
+            {
+                "abc",
+                "ijk",
+                "xyz"
+            };
+
+            foreach (var item in TransformList(OldList))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        private static List<string> TransformList(List<string> stringList)
+        {
+            var resultArray = new string[stringList.Count];
+            var newList = new List<string>();
+
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                for (int j = 0; j < stringList.Count; j++)
+                {
+                    resultArray[i] += stringList[j][i];
+                }
+
+                newList.Add(resultArray[i]);
+            }
+
+            return newList;
+        }
+    }
+}
